40a9a2731f8dbc295dc69a38a62b90749b8e0581
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-16 01:10:28 +0000

    the tests for signing, works with Smart contracts

--- test/testSign.js
+++ test/testSign.js
@@ -1,10 +1,10 @@
-const { sign } = require("../web3/sign");
+const { signAirdrop } = require("../web3/sign");
 const ethUtil = require("ethereumjs-util");
 require("dotenv").config();
 
 (function test1() {
   console.log("Testing signing");
-  const [sig, address] = sign(
+  const [sig, address] = signAirdrop(
     "1234567891234",
     "12",
     "0xDF16399E6F10bbC1C07C88c6c70116182FA2e118"
@@ -20,7 +20,7 @@ require("dotenv").config();
   console.log(
     "Testing signing with different address to match the smart contract tests"
   );
-  const [sig, address] = sign(
+  const [sig, address] = signAirdrop(
     "1234567891212121",
     "1000000000",
     "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
