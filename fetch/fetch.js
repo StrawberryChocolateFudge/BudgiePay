de573fdda0d9c43a33aefd7792a77c36fd806d2b
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-16 01:08:53 +0000

    added tweet fetching

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,10 +1,18 @@
 const axios = require("axios");
 
-module.exports = { fetchfollowers };
+module.exports = { fetchfollowers, fetchTweets };
 
 async function fetchfollowers(subject) {
   return axios({
     url: `https://api.twitter.com/1.1/users/show.json?user_id=${subject}`,
+    method: "get",
+    headers: { Authorization: `Bearer ${process.env["BEARERTOKEN"]}` },
+  });
+}
+
+async function fetchTweets(subject) {
+  return axios({
+    url: `https://api.twitter.com/2/users/${subject}/tweets`,
     method: "get",
     headers: { Authorization: `Bearer ${process.env["BEARERTOKEN"]}` },
   });
