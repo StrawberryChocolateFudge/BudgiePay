9e5d97ea735106eb1910c4f82ec6031be6ba15d7
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-17 23:31:46 +0000

    added verifySignature, but not used

--- public/js/main.js
+++ public/js/main.js
@@ -24,6 +24,7 @@ if (withdrawBttn !== null) {
       const signature = JSON.parse(withdrawBttn.dataset.signature);
       const artifact = await (await fetch("/js/thefollowertoken.json")).text();
       const abi = JSON.parse(artifact).abi;
+
       await mintFollowerToken(
         signedargs,
         signature,
@@ -56,6 +57,30 @@ async function getAddress() {
   const web3 = new Web3(window.ethereum);
   const accounts = await web3.eth.getAccounts();
   return accounts[0];
+}
+
+async function verifySignature(
+  signedargs,
+  signature,
+  contractaddress,
+  abi,
+  address
+) {
+  const web3 = new Web3(window.ethereum);
+  const contract = new web3.eth.Contract(abi, contractaddress);
+  console.log(signedargs);
+  const res = await contract.methods
+    .verifySignature(
+      signature.v,
+      signature.r,
+      signature.s,
+      signedargs.twitterid,
+      signedargs.followerscount,
+      signedargs.address
+    )
+    .call({ from: address });
+
+  console.log(res);
 }
 
 async function mintFollowerToken(
