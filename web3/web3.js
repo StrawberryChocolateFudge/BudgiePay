c7a505069e902960d6ed59b79691d2dd2096a4d0
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-22 02:10:37 +0000

    added paymentcontract

--- web3/web3.js
+++ web3/web3.js
@@ -1,18 +1,45 @@
 const Web3 = require("web3");
-const abi = require("./abi");
-module.exports = { getWithdrawnAlready };
+const { budgeCoinAbi, paymentsAbi } = require("./abi");
+const { keccak256 } = require("ethereumjs-util");
 
-function getWeb3() {
-  return new Web3(process.env["RPC"]);
+let web3;
+
+function getWeb3(rpc) {
+  if (web3 === undefined) {
+    web3 = new Web3(rpc);
+  }
+  return web3;
 }
 
-async function getContract() {
-  const web3 = getWeb3();
-
-  return await new web3.eth.Contract(abi(), process.env["CONTRACTADDRESS"]);
+async function getBudgieCoinContract() {
+  const rpc = process.env["RPC"];
+  const tokenAddress = process.env["TOKENADDRESS"];
+  const web3 = getWeb3(rpc);
+  return await new web3.eth.Contract(budgeCoinAbi(), tokenAddress);
 }
 
 async function getWithdrawnAlready(twitterId) {
-  const contract = await getContract();
+  const contract = await getBudgieCoinContract();
   return await contract.methods.getWithdrawnAlready(twitterId).call();
 }
+
+async function getPaymentContract() {
+  const web3 = getWeb3(process.env["RPC"]);
+  const paymentTokenAddress = process.env["PAYMENTCONTRACTADDRESS"];
+  return await new web3.eth.Contract(paymentsAbi(), paymentTokenAddress);
+}
+
+async function getPaymentById(id) {
+  const contract = await getPaymentContract();
+  return await contract.methods.getPaymentById(id).call();
+}
+
+async function hashTwitterId(twitterId) {
+  return keccak256(Buffer.from(twitterId));
+}
+
+module.exports = {
+  getWithdrawnAlready,
+  getPaymentById,
+  hashTwitterId,
+};
