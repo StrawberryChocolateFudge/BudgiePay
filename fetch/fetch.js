653070023fae2afb798f49fef8b5f4eec714b592
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-22 02:04:39 +0000

    added fetchuser by handle

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-module.exports = { fetchfollowers, fetchTweets };
+module.exports = { fetchfollowers, fetchTweets, fetchUserByHandle };
 
 async function fetchfollowers(subject) {
   return axios({
@@ -16,4 +16,19 @@ async function fetchTweets(subject) {
     method: "get",
     headers: { Authorization: `Bearer ${process.env["BEARERTOKEN"]}` },
   });
+}
+
+async function fetchUserByHandle(handle) {
+  let res;
+  try {
+    res = await axios({
+      method: "get",
+      url: `https://api.twitter.com/2/users/by/username/${handle}`,
+      headers: { Authorization: `Bearer ${process.env["BEARERTOKEN"]}` },
+    });
+  } catch (err) {
+    console.log("ERROR ");
+    res = "Error Occured";
+  }
+  return res;
 }
