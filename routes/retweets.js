12bc4a6413abc747e28567349c7f37619beb4ddd
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-22 02:08:04 +0000

    removed

--- routes/retweets.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* GET retweets page. */
-router.get("/", function (req, res, next) {
-  res.render("retweets", { user: req.user });
-});
-
-module.exports = router;
